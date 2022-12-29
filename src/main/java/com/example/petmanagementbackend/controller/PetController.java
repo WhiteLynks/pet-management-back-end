20aeca3ca75de5b12f91c2ca86ee8253cc8530f4
Asko Vendel <asvend@ttu.ee>
2022-12-29 07:45:00 +0200

    Fix PetController

--- src/main/java/com/example/petmanagementbackend/controller/PetController.java
+++ src/main/java/com/example/petmanagementbackend/controller/PetController.java
@@ -3,14 +3,12 @@ package com.example.petmanagementbackend.controller;
 import com.example.petmanagementbackend.model.Pet;
 import com.example.petmanagementbackend.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class PetController {
 
     @Autowired
@@ -21,7 +19,7 @@ public class PetController {
         return petRepo.findAll();
     }
 
-    @PostMapping("/pets/add")
+    @PostMapping("/pets")
     public Pet AddPet(@RequestBody Pet newPet) {
         petRepo.save(newPet);
         return newPet;
